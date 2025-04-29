0b17c8e615fe92ccaecf01a11ae9aa77d71224a6
user <mariiaantoniv@gmail.com>
2025-04-29 12:38:22 +0300

    make build

---
+++ index.91ff2801.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");t.style.top=Math.round(e.clientHeight/2-t.offsetHeight/2)+"px",t.style.left=Math.round(e.clientWidth/2-t.offsetWidth/2)+"px";
+//# sourceMappingURL=index.91ff2801.js.map
